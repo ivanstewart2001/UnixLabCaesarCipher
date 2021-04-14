d5448fb07fff472f9af9891c229a63a6628a813c
Ivan G Stewart <71293769+ivanstewart2001@users.noreply.github.com>
2021-04-14 12:22:34 -0400

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+def encrypt(inp, shift):
+    inp = inp.upper()
+    answer = ""
+    for i in range(len(inp)):
+        letter = inp[i]
+        
+        if (letter.isupper()):
+            answer += chr((ord(letter) + shift - 65) % 26 + 65)
+    return answer.replace('o',"").upper()
