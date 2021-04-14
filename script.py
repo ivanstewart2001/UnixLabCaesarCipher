20fe9b79e28397751401288345bff3cf3faa8613
Ivan G Stewart <71293769+ivanstewart2001@users.noreply.github.com>
2021-04-14 12:26:01 -0400

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-def encrypt(inp, shift):
-    inp = inp.upper()
-    answer = ""
-    for i in range(len(inp)):
-        letter = inp[i]
-        
-        if (letter.isupper()):
-            answer += chr((ord(letter) + shift - 65) % 26 + 65)
-    return answer.replace('o',"").upper()
